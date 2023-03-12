7742e31d92630e12ffe33bb2796b514871f0586b
unknown <elhamhashemi56@yahoo.com>
2023-03-12 20:35:08 +0100

    range grade and color

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -28,18 +28,41 @@ const TodoList=()=>{
             <div class="d-flex mb-3 flex_Container">
                 <div class="p-2 flex-fill">
                     <label for="disabledTextInput" class="form-label">Grade :</label>
-                    <fieldset disabled>
-                       <input type="disabled" id="disabledTextInput" class="form-control disableInput hh" placeholder={counter} />
-                    </fieldset>
+                    {(counter===1 || counter===2) &&
+                        <fieldset disabled className=" greenColor" >
+                            {counter}
+                        </fieldset>
+                    }
+
+                    {counter===3  &&
+                        <fieldset disabled className=" yellowColor" >
+                            {counter}
+                        </fieldset>
+                    }
+
+                    {counter===4 &&
+                        <fieldset disabled className=" orangColor" >
+                            {counter}
+                        </fieldset>
+                    }
+
+                    {(counter===5 || counter===6) &&
+                        <fieldset disabled className=" redColor" >
+                            {counter}
+                        </fieldset>
+                    }
+
+                   
+
                 </div>
                 <div class="p-2 align-self-end ">
                     
-                    {counter === 6 ? <button type="button" class="btn btn-secondary " disabled={isDisabled} >Inc</button>
-                                    :<button type="button" class="btn btn-danger " onClick={inc}>Inc</button>
+                    {counter === 6 ? <button type="button" class="btn btn-secondary " disabled={isDisabled} >+Inc</button>
+                                    :<button type="button" class="btn btn-danger " onClick={inc}>+Inc</button>
                     }
 
-                    {counter === 1 ?<button type="button" class="btn btn-secondary ml-3 mr-3" disabled={isDisabled} >Dec</button>                    
-                                    :<button type="button" class="btn btn-danger ml-3 mr-3" onClick={dec}>dec</button>
+                    {counter === 1 ?<button type="button" class="btn btn-secondary ml-3 mr-3" disabled={isDisabled} >-Dec</button>                    
+                                    :<button type="button" class="btn btn-danger ml-3 mr-3" onClick={dec}>-Dec</button>
                     }
                     
                 </div>
